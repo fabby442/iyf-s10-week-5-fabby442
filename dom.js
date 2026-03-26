46ddfcaf7b8658349ea13c543fc7f8c3288c2821
fabby <fabianmunga163@gmail.com>
2026-03-26 09:26:51 +0000

    Select form using getElementById

---
+++ dom.js
@@ -0,0 +1,23 @@
+// getElementById example
+const header = document.getElementById("main-header");
+console.log(header);
+// ===== MY PRACTICE =====
+
+// 1. Select h1
+const h1 = document.querySelector("h1");
+console.log("H1:", h1);
+
+// 2. Select all content
+const contents = document.getElementsByClassName("content");
+console.log("Contents:", contents);
+
+// 3. Select form
+const form = document.getElementById("contact-form");
+console.log("Form:", form);
+
+// 4. Select email input
+const email = document.querySelector('input[type="email"]');
+console.log("Email:", email);
+// Select form using getElementById
+const form = document.getElementById("contact-form");
+console.log(form);
